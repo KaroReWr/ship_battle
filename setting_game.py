bd2231107bd907ece95c888cd7d07532231c8e82
[KaroEr] <[jak-anna@wp.pl]>
2019-11-15 21:02:04 +0100

    function to add coordinate on board and place part of the ship

--- setting_game.py
+++ setting_game.py
@@ -2,15 +2,18 @@ from ship_factory import Ship
 from battle_board_creation import Board
 
 karo_board = Board(0,10)
+karo_board.new_board()
+armada = [[[1, 1], [2, 1], [3, 1], [4, 1], [5, 1]], [[7, 7], [7, 8], [7, 9], [7, 10], [7, 11], [7, 12], [7, 13]]]
 
+def add_pack_of_the_ships(board,armada):
+    for ships in armada:
+        for ship in ships:
+            for coord in ship:
+                if board[ships][ship][coord] == ".":
+                    board[ships][ship][coord] = "S"
+                else:
+                    print("Field not empty")
+    return board
 
-def add_pack_of_the_ships(board,pack):
-    for ship in pack:
-        for coordinates in ship:
-            if board[i][j] == ".":
-                board[x][y] = "S"
-            else:
-                print("Field not empty")
-
- if __name__ == '__main__':
-     print(karo_board.shoot_return_field())
+if __name__ == '__main__':
+     print(add_pack_of_the_ships(karo_board,armada)
