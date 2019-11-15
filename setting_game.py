b97af4a273bb8eff4a5c682106e50f4d0f3d299a
[KaroEr] <[jak-anna@wp.pl]>
2019-11-15 21:57:18 +0100

    modification function for adding ships on a board

--- setting_game.py
+++ setting_game.py
@@ -3,17 +3,16 @@ from battle_board_creation import Board
 
 karo_board = Board(0,10)
 karo_board.new_board()
-armada = [[[1, 1], [2, 1], [3, 1], [4, 1], [5, 1]], [[7, 7], [7, 8], [7, 9], [7, 10], [7, 11], [7, 12], [7, 13]]]
+armada = [[[1, 1], [2, 1], [3, 1], [4, 1], [5, 1]], [[7, 7], [7, 8], [7, 9]]]
 
 def add_pack_of_the_ships(board,armada):
-    for ships in armada:
-        for ship in ships:
-            for coord in ship:
-                if board[ships][ship][coord] == ".":
-                    board[ships][ship][coord] = "S"
-                else:
-                    print("Field not empty")
-    return board
+    for ship in armada:
+        for coord in ship:
+            x = coord[0]
+            y = coord[1]
+            board.add_coordinate(x,y)
+
 
 if __name__ == '__main__':
-     print(add_pack_of_the_ships(karo_board,armada)
+     print(add_pack_of_the_ships(karo_board,armada))
+     karo_board.print_water_board()
