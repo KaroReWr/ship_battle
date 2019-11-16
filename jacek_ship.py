849ed2b89ca4f8cad029cc4f39ac7d28e9ddcefc
[KaroEr] <[jak-anna@wp.pl]>
2019-11-16 15:12:38 +0100

    separate file with oponent's coordinates of ships

--- jacek_ship.py
+++ jacek_ship.py
@@ -1,4 +1,1 @@
-from battle_board_creation import Board
 armada_jacek = [[[0, 0], [0, 1], [0, 2], [0, 3]], [[2, 0], [2, 1], [2, 2]], [[4, 0], [4, 1], [4, 2]], [[6, 0], [6, 1]], [[8, 0], [8, 1]], [[6, 2], [6, 3]], [[0, 9]], [[9, 4]], [[9, 6]], [[9, 8]]]
-jacek_board = Board(0,10)
-jacek_board.new_board()
