81325a1f443c666be552b1f8d254eb54f884b48e
[KaroEr] <[jak-anna@wp.pl]>
2019-11-16 15:11:18 +0100

    import armada from jacek_ship file and set my and oponent board(shadowed)

--- setting_game.py
+++ setting_game.py
@@ -1,12 +1,14 @@
 from ship_factory import Ship
 from battle_board_creation import Board
+from jacek_ship import armada_jacek
 
-#from jacek_ship import *
 
 karo_board = Board(0,10)
 karo_board.new_board()
 armada_karo = [[[0, 0]], [[0, 9]], [[9, 9]], [[7, 8]],[[2, 8], [2, 9]], [[5, 6], [6, 6]], [[4, 4], [5, 4]],[[8, 2], [8, 3], [8, 4]], [[2, 0],[3, 0],[4, 0]], [[6, 0], [7, 0], [8, 0], [9, 0]]]
 
+jacek_board = Board(0,10)
+jacek_board.new_board()
 
 
 def add_pack_of_the_ships(board,armada):
@@ -20,11 +22,10 @@ def add_pack_of_the_ships(board,armada):
 
 if __name__ == '__main__':
     add_pack_of_the_ships(karo_board,armada_karo)
-    karo_board.bingo(0,0)
-    karo_board.print_shadow_board()
+    add_pack_of_the_ships(jacek_board,armada_jacek)
+    karo_board.print_water_board()
     print(" " * 20)
     print("-" * 30)
     print(" " * 20)
-    karo_board.print_water_board()
-
+    jacek_board.print_shadow_board()
 
